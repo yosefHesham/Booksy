1b1cc883f4bb179732957de5d80f24fb355e2709
yosefHesham <yousefmobdev@gmail.com>
2022-08-23 17:12:26 +0200

    Style book

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -4,22 +4,27 @@ import useGetBooks from "../hooks/useGetBooks";
 const BookList = () => {
   const [data, error, loading, pageEnd] = useGetBooks();
   return (
-    <section className="d-grid">
+    <section className="d-grid  mt-8 w-4/5 mx-auto">
       {" "}
       {data.map((book, i) => {
         return (
-          <article ref={i === data.length - 1 ? pageEnd : null}>
+          <article
+            className="max-h-fit"
+            ref={i === data.length - 1 ? pageEnd : null}
+          >
             <img
-              className="rounded-md  h-56"
+              className="rounded-md h-4/5"
               src={book.formats["image/jpeg"]}
               alt={book.title}
             />
 
-            <p>{book.title}</p>
-            <p>{book.authors.map((author) => author.name).join(", ")}</p>
-            {[1, 2, 3, 4].map((n) => (
-              <FontAwesomeIcon icon="fa-solid fa-star" color="yellow" />
-            ))}
+            <div className="h-1/4 flex-col ">
+              <p className="max-lines">{book.title.split(":" || ";")[0]}</p>
+              <p className="max-lines">{book.authors[0]?.name}</p>
+              {[1, 2, 3, 4].map((n) => (
+                <FontAwesomeIcon icon="fa-solid fa-star" color="yellow" />
+              ))}
+            </div>
           </article>
         );
       })}{" "}
